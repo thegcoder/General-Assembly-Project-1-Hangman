80f24f96ab67f514b3d89e2a07451521ad64330c
Tai Glover <taigloverslaton@gmail.com>
2019-06-12 18:39:44 -0400

    added more js code

--- hangman.js
+++ hangman.js
@@ -2,4 +2,12 @@ window.onload = function () {
 
     var alphabet = [ 'a', 'b', 'c', 'd','e','f','g','h','i','j','k','l','m','n', 'o','p','q','r','s','t','u','v','w','x','y','z'];
 
+   //store guess
+   var guesses [ ];
+   //count wrong guesses
+   var counter ;
+   //Number of spaces in each word
+   var space;
+   //choose category
     
+} 
