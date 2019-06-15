c4a9634beab5eda78c2d233b5d01ac699482f94a
Tai Glover <taigloverslaton@gmail.com>
2019-06-15 15:23:53 -0400

    reorganize html format

--- hangman.js
+++ hangman.js
@@ -2,7 +2,7 @@ window.onload = function () {
 
     var alphabet = [ 'a', 'b', 'c', 'd','e','f','g','h','i','j','k','l','m','n', 'o','p','q','r','s','t','u','v','w','x','y','z'];
 
-   //store guess
+   //store guesses
    var guesses [ ];
    //count wrong guesses
    var counter ;
